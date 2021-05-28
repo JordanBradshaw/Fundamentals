bae18f1f84e72b962fd57026ecdf4e46bb4a6c79
Jordan Bradshaw <jordanbradshaw27@aol.com>
2021-05-28 13:48:06 -0700

    +q1c++

---
+++ leetCode/c++/question1.cpp
@@ -0,0 +1,30 @@
+/*
+1. Two Sum
+
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+
+Runtime: 444 ms, faster than 5.10% of C++ online submissions for Two Sum.
+Memory Usage: 10.1 MB, less than 25.53% of C++ online submissions for Two Sum.
+*/
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> retVec;
+        for (int i=0; i<nums.size(); i++) {
+            for (int j=i+1; j<nums.size(); j++) {
+                if (nums[i]+nums[j]==target) {
+                    retVec.push_back(i);
+                    retVec.push_back(j);
+                }
+            }
+        }
+        return retVec;
+    }
+};
